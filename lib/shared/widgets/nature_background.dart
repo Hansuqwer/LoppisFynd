cc464310b09c5ddd4dd15e84c77a1ea40555fbdc
HansuQWER <HansuQWER@protonmail.com>
2026-06-08 11:24:21 +0200

    Use new ChatGPT background image

--- lib/shared/widgets/nature_background.dart
+++ lib/shared/widgets/nature_background.dart
@@ -11,7 +11,7 @@ class NatureBackground extends StatelessWidget {
   const NatureBackground({super.key});
 
   static const _backgroundImage =
-      'Images/Background/background_photo.jpg';
+      'Images/Background/background_photo.png';
 
   @override
   Widget build(BuildContext context) {
